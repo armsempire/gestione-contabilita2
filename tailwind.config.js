66e491eaae09446c9aacff37a18c565530866511
armsempire <armsempire.com@gmail.com>
2025-02-22 00:32:43 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
-
+// tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',  // Includi tutte le pagine e i componenti
+    './components/**/*.{js,ts,jsx,tsx}', // Includi anche i componenti
   ],
   theme: {
     extend: {},
