1cce1f43c13156b5c49e14298e5f343e84d7b3d1
armsempire <armsempire.com@gmail.com>
2025-02-21 23:30:14 +0100

    Aggiornati stili globali e configurazione app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import "../styles/globals.css";
+// pages/_app.js
+import '../styles/globals.css' // Importa gli stili globali
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component {...pageProps} />
 }
